f95f5b823d0c316144934faffb60b125a39caf5a
v1laris <25050147@study.bsuir.by>
2025-03-15 11:42:21 +0300

    Сервер теперь выводит свои возможные ip адреса (упрощает подключение)

--- src/main/java/org/example/TCPServer.java
+++ src/main/java/org/example/TCPServer.java
@@ -1,8 +1,11 @@
 package org.example;
 
 import java.io.*;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,6 +23,18 @@ public class TCPServer {
         }
 
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = interfaces.nextElement();
+                Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    InetAddress address = addresses.nextElement();
+                    // Фильтруем loopback-адреса (127.0.0.1) и проверяем, что это IPv4
+                    if (!address.isLoopbackAddress() && address.getAddress().length == 4) {
+                        System.out.println("Серверный IP-адрес: " + address.getHostAddress());
+                    }
+                }
+            }
             System.out.println("Сервер запущен на порту " + PORT);
 
             while (true) {
